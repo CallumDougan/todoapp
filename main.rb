2bd6890c366c6b009b04c7cac9be3dcc7d4020f5
CallumDougan <practicalitymalfunction@gmail.com>
2015-12-02 16:11:58 +0000

    adding and listing tasks functional

--- main.rb
+++ main.rb
@@ -4,15 +4,25 @@ require 'sinatra/contrib/all' if development?
 require 'pg'
 
 get '/tasks' do
-  # get all tasks from DB
+  sql = "SELECT * FROM tasks"
+  @tasks = run_sql(sql)
+  erb :index
 end
 
 get '/tasks/new' do
   # get form to create new task with
+  erb :new
 end
 
 post '/tasks' do
   # create new task, persistent in DB
+  # binding.pry
+  name = params[:name]
+  details = params[:details]
+
+  sql = "INSERT INTO tasks (name, details) VALUES ('#{name}', '#{details}')"
+  run_sql(sql)
+  redirect to('/tasks')
 end
 
 get '/tasks/:id' do
@@ -29,4 +39,11 @@ end
 
 post '/tasks/:id/delete' do
   # destroy in DB
+end
+
+def run_sql(sql)
+  conn = PG.connect(dbname: 'to_do', host: 'localhost')
+  result = conn.exec(sql)
+  conn.close
+  result
 end
